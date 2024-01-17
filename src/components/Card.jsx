678a7c27a9fe114511068250b489f64aedc94a99
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-01-17 11:45:02 -0500

    Build logic for 3d card rotation on mouse move

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,7 +9,7 @@ export default function Card({
   gameOver,
   delay,
 }) {
-  const cardRef = useRef(); // ref '.card' wrapper
+  const cardRef = useRef(null); // ref '.card' wrapper
 
   // on re-render: flip card, wait delay ms, flip card again
   useEffect(() => {
@@ -23,8 +23,46 @@ export default function Card({
     setTimeout(flipCard, delay);
   });
 
+  // change Card's transform value based on mouse position,
+  // causing it to 3d rotate to pull toward mouse
+  function handleMouseMove(e) {
+    requestAnimationFrame(() => {
+      console.log('mousemove');
+      cardRef.current.style.transform = getTransforms(
+        e.clientX,
+        e.clientY,
+        cardRef.current,
+      );
+    });
+  }
+
+  // based on given x & y mouse coords and element, calculate and return transform value
+  // e.g. 'transform: perspective(1000px) rotateX(-40.7617deg) rotateY(-33.0273deg)'
+  function getTransforms(x, y, el) {
+    const constraint = 2; // constrain rotation by this factor
+    const box = el.getBoundingClientRect();
+    const rotateX = (y - box.y - box.height / 2) / constraint;
+    const rotateY = -(x - box.x - box.width / 2) / constraint;
+
+    return `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  }
+
+  // reset Card transform value to 0deg (default)
+  function handleMouseLeave() {
+    setTimeout(() => {
+      console.log('mouseleave');
+      cardRef.current.style.transform = 'rotate(0deg)';
+    }, 10);
+  }
+
   return (
-    <div ref={cardRef} className="card" onClick={() => handleSelection(name)}>
+    <div
+      ref={cardRef}
+      className="card"
+      onClick={() => handleSelection(name)}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+    >
       <div className="card-inner">
         <div
           className="card-front"
