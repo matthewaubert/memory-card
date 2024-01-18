ea8ac34c336cc164d0e3b4782878d0973837e4e5
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-01-17 23:02:02 -0500

    Build logic for card glare effect on mouse move

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,7 +9,8 @@ export default function Card({
   gameOver,
   delay,
 }) {
-  const cardRef = useRef(null); // ref '.card' wrapper
+  const cardRef = useRef(null); // ref '.card' div
+  const glareRef = useRef(null); // ref '.glare' div
 
   // on re-render: flip card, wait delay ms, flip card again
   useEffect(() => {
@@ -23,36 +24,86 @@ export default function Card({
     setTimeout(flipCard, delay);
   });
 
+  function handleMouseOver() {
+    addWillChange(); // hint to browser to optimize changes
+    // console.log('mouseover');
+  }
+
   // change Card's transform value based on mouse position,
   // causing it to 3d rotate to pull toward mouse
   function handleMouseMove(e) {
     requestAnimationFrame(() => {
-      console.log('mousemove');
+      // set card transform value
       cardRef.current.style.transform = getTransforms(
         e.clientX,
         e.clientY,
         cardRef.current,
       );
+
+      // set glare background and opacity
+      const glare = getGlare(
+        e.clientX,
+        e.clientY,
+        glareRef.current,
+      );
+      glareRef.current.style.background = glare.background;
+      glareRef.current.style.opacity = glare.opacity;
+      // console.log('mousemove');
     });
   }
 
-  // based on given x & y mouse coords and element, calculate and return transform value
-  // e.g. 'transform: perspective(1000px) rotateX(-40.7617deg) rotateY(-33.0273deg)'
-  function getTransforms(x, y, el) {
-    const constraint = 2; // constrain rotation by this factor
+  // based on given x & y mouse coords and element, calc and return transform value
+  // e.g. 'perspective(1000px) rotateX(-40.7617deg) rotateY(-33.0273deg)'
+  function getTransforms(mouseX, mouseY, el) {
+    const constraint = 6; // constrain rotation by this factor
     const box = el.getBoundingClientRect();
-    const rotateX = (y - box.y - box.height / 2) / constraint;
-    const rotateY = -(x - box.x - box.width / 2) / constraint;
+    // calc rotation in degrees of x and y axes
+    const rotateX = (mouseY - box.y - box.height / 2) / constraint;
+    const rotateY = -(mouseX - box.x - box.width / 2) / constraint;
 
     return `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  }
+
+  // based on given x & y mouse coords and element,
+  // calc and return obj containing glare background and opacity
+  // e.g. {
+  //   background: radial-gradient(circle at 119.406px 225px, #fffa 40px, #0000),
+  //   opacity: 0.867583,
+  // }
+  function getGlare(mouseX, mouseY, el) {
+    const box = el.getBoundingClientRect();
+    // calc x and y coords of glare radial gradient
+    const posX = mouseX - box.x;
+    const posY = mouseY - box.y;
+    // set opacity to 0 btw top and mid of y axis, range 0 - 1 btw mid and bottom
+    const opacity = Math.max(0, (posY / box.height) * 2 - 1);
+
+    return {
+      background: `radial-gradient(circle at ${posX}px ${posY}px, #fffa 40px, #0000)`,
+      opacity,
+    };
   }
 
   // reset Card transform value to 0deg (default)
   function handleMouseLeave() {
     setTimeout(() => {
-      console.log('mouseleave');
       cardRef.current.style.transform = 'rotate(0deg)';
+      glareRef.current.style.opacity = 0;
+      removeWillChange(); // remove optimization
+      // console.log('mouseleave');
     }, 10);
+  }
+
+  // hint to browser how these elements will change (to optimize)
+  function addWillChange() {
+    cardRef.current.style.willChange = 'transform';
+    glareRef.current.style.willChange = 'opacity';
+  }
+
+  // remove hint to browser (to free up resources)
+  function removeWillChange() {
+    cardRef.current.style.willChange = 'auto';
+    glareRef.current.style.willChange = 'auto';
   }
 
   return (
@@ -60,6 +111,7 @@ export default function Card({
       ref={cardRef}
       className="card"
       onClick={() => handleSelection(name)}
+      onMouseOver={handleMouseOver}
       onMouseMove={handleMouseMove}
       onMouseLeave={handleMouseLeave}
     >
@@ -73,6 +125,7 @@ export default function Card({
           <h3 className="name">{name}</h3>
         </div>
         <div className="card-back"></div>
+        <div ref={glareRef} className="glare"></div>
       </div>
     </div>
   );
