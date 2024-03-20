99901296cdd1e3e4f55a98733e50cc07b809d27f
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-03-19 23:57:10 -0400

    Move funcs outside Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -46,38 +46,6 @@ export default function Card({
     });
   }
 
-  // based on given x & y mouse coords and element, calc and return CSS transform value
-  // e.g. 'perspective(1000px) rotateX(-40.7617deg) rotateY(-33.0273deg)'
-  function getTransforms(mouseX, mouseY, el) {
-    const constraint = 6; // constrain rotation by this factor
-    const box = el.getBoundingClientRect();
-    // calc rotation in degrees of x and y axes
-    const rotateX = (mouseY - box.y - box.height / 2) / constraint;
-    const rotateY = -(mouseX - box.x - box.width / 2) / constraint;
-
-    return `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-  }
-
-  // based on given x & y mouse coords and element,
-  // calc and return obj containing CSS glare background and opacity
-  // e.g. {
-  //   background: radial-gradient(circle at 119.406px 225px, #fffa 40px, #0000),
-  //   opacity: 0.867583,
-  // }
-  function getGlare(mouseX, mouseY, el) {
-    const box = el.getBoundingClientRect();
-    // calc x and y coords of glare radial gradient
-    const posX = mouseX - box.x;
-    const posY = mouseY - box.y;
-    // set opacity to 0 btw top and mid of y axis, range 0 - 1 btw mid and bottom
-    const opacity = Math.max(0, (posY / box.height) * 2 - 1);
-
-    return {
-      background: `radial-gradient(circle at ${posX}px ${posY}px, #fffa 40px, #0000)`,
-      opacity,
-    };
-  }
-
   // reset Card CSS transform value to 0deg (default)
   function handleMouseLeave() {
     setTimeout(() => {
@@ -122,4 +90,36 @@ export default function Card({
       </div>
     </div>
   );
+}
+
+// based on given x & y mouse coords and element, calc and return CSS transform value
+// e.g. 'perspective(1000px) rotateX(-40.7617deg) rotateY(-33.0273deg)'
+function getTransforms(mouseX, mouseY, el) {
+  const constraint = 6; // constrain rotation by this factor
+  const box = el.getBoundingClientRect();
+  // calc rotation in degrees of x and y axes
+  const rotateX = (mouseY - box.y - box.height / 2) / constraint;
+  const rotateY = -(mouseX - box.x - box.width / 2) / constraint;
+
+  return `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+}
+
+// based on given x & y mouse coords and element,
+// calc and return obj containing CSS glare background and opacity
+// e.g. {
+//   background: radial-gradient(circle at 119.406px 225px, #fffa 40px, #0000),
+//   opacity: 0.867583,
+// }
+function getGlare(mouseX, mouseY, el) {
+  const box = el.getBoundingClientRect();
+  // calc x and y coords of glare radial gradient
+  const posX = mouseX - box.x;
+  const posY = mouseY - box.y;
+  // set opacity to 0 btw top and mid of y axis, range 0 - 1 btw mid and bottom
+  const opacity = Math.max(0, (posY / box.height) * 2 - 1);
+
+  return {
+    background: `radial-gradient(circle at ${posX}px ${posY}px, #fffa 40px, #0000)`,
+    opacity,
+  };
 }
